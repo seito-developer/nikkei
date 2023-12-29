0a6090078d4901429141343e1d1324a46f6e89b0
seito-developer <seito.horiguchi@gmail.com>
2023-12-29 15:54:04 +0900

    added demo

--- demo/counter/app.js
+++ demo/counter/app.js
@@ -1,5 +1,6 @@
+const $counter = document.getElementById('js-counter');
+
 document.getElementById('js-buttton').addEventListener('click', () => {
-    const $counter = document.getElementById('js-counter');
     let currentCount = parseInt($counter.textContent);
     $counter.textContent = currentCount + 1;
 });
